81b5414858050389a277154bfd3a5cb32102e782
theo24-sys <timothybanjo42@gmail.com>
2026-03-19 21:54:08 +0300

    bypass manifest map parsing

--- accounts/views.py
+++ accounts/views.py
@@ -35,7 +35,7 @@ def register_view(request):
             form = StudentRegisterForm(request.POST, request.FILES)
         if form.is_valid():
             user = form.save()
-            login(request, user)
+            login(request, user, backend='django.contrib.auth.backends.ModelBackend')
             messages.success(request, f"Welcome to GradPulse, {user.display_name}!")
             if user.is_employer:
                 return redirect('corporate_dashboard')
