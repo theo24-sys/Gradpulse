dd328aa1d66a502acb7c61d11c0ddd85b4116bc0
theo24-sys <timothybanjo42@gmail.com>
2026-03-21 06:43:42 +0300

    fix: Jazzmin UI tweaks deprecation and prepare for subsuperuser test

--- gradpulse/settings.py
+++ gradpulse/settings.py
@@ -364,7 +364,7 @@ JAZZMIN_UI_TWEAKS = {
     "sidebar_nav_legacy_style": False,
     "sidebar_nav_flat_style": False,
     "theme": "slate",
-    "dark_mode_theme": "slate",
+    "default_theme_mode": "auto",
     "button_classes": {
         "primary": "btn-primary",
         "secondary": "btn-secondary",
